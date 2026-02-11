cc6ca746859b72107f0324b19d77cb742cd9a6dd
Robin Deplanques <robin.deplanques@abtasty.com>
2026-02-11 22:44:06 +0100

    fix(buildozer): use PythonRecipe for sqlalchemy (skip C ext build)
    CompiledComponentsPythonRecipe fails on SQLAlchemy 2.x cross-compile.
    Switch to PythonRecipe for pure Python mode installation. C extensions
    are optional for SQLAlchemy.

--- p4a-recipes/sqlalchemy/__init__.py
+++ p4a-recipes/sqlalchemy/__init__.py
@@ -1,10 +1,16 @@
-from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+from pythonforandroid.recipe import PythonRecipe
 
 
-class SQLAlchemyRecipe(CompiledComponentsPythonRecipe):
+class SQLAlchemyRecipe(PythonRecipe):
+    """SQLAlchemy 2.x recipe (pure Python mode, skip C extensions)."""
+
     name = "sqlalchemy"
     version = "2.0.36"
-    url = "https://files.pythonhosted.org/packages/50/65/9cbc9c4c3287bed2499e05033e207473504dc4df999ce49385fb1f8b058a/sqlalchemy-{version}.tar.gz"
+    url = (
+        "https://files.pythonhosted.org/packages/50/65/"
+        "9cbc9c4c3287bed2499e05033e207473504dc4df999ce49385fb1f8b058a/"
+        "sqlalchemy-{version}.tar.gz"
+    )
     call_hostpython_via_targetpython = False
     depends = ["setuptools"]
 
